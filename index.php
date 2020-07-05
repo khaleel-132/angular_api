1f909856e25fe4dac79ddcc1486633469ffb62e6
khaleel-132 <khaleel.alungal132@gmail.com>
2020-07-05 21:00:08 +0530

    api

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
+</head>
+<body ng-app='myApp'>
+<a href="#/!"></a>
+
+   <div ng-view></div> 
+<script src="app/route.js"></script>
+</body>
+</html>
